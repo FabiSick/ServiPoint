cfa79161d28dc5c759c97449b0030438ac6e196d
FabiSick <fabianignaciont@gmail.com>
2024-09-08 21:58:45 -0300

    Angula y Boostrap

--- src/app/reset-password/reset-password.module.ts
+++ src/app/reset-password/reset-password.module.ts
@@ -3,17 +3,24 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
 import { ResetPasswordPageRoutingModule } from './reset-password-routing.module';
-
 import { ResetPasswordPage } from './reset-password.page';
+
+// Importar módulos de Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ResetPasswordPageRoutingModule
+    ResetPasswordPageRoutingModule,
+    MatButtonModule, // Importar MatButtonModule
+    MatInputModule, // Importar MatInputModule
+    MatFormFieldModule, // Importar MatFormFieldModule
   ],
   declarations: [ResetPasswordPage]
 })
