cb54723f79952ae72362ca86674a8914e9bd1dba
Carlamejias <102421609+Carlamejias@users.noreply.github.com>
2024-11-25 17:55:27 -0300

    JJ

--- src/app/chat-en-vivo/chat-en-vivo.page.ts
+++ src/app/chat-en-vivo/chat-en-vivo.page.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class ChatEnVivoPage implements OnInit {
 
   constructor() { }
+  abrirWhatsApp() {
+    // Número de WhatsApp Business (cambiar por el número deseado)
+    const phoneNumber = '56934953492'; // Código de país (+56 para Chile) seguido del número (sin símbolos)
+
+    // Mensaje opcional predefinido
+    const message = 'Gracias por comunicarte con ServiPoint. Por favor, haznos saber cómo podemos ayudarte.';
+
+    // Enlace completo de WhatsApp
+    const url = `https://wa.me/${56934953492}?text=${encodeURIComponent(message)}`;
+
+    // Redirigir al enlace de WhatsApp
+    window.open(url, '_blank');
+  }
 
   ngOnInit() {
   }
--- src/app/configuracion/configuracion.page.ts
+++ src/app/configuracion/configuracion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configuracion',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfiguracionPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // Método para cambiar notificaciones
+  cambiarNotificaciones() {
+    console.log('Cambio de notificaciones');
   }
 
+  // Método para cambiar idioma
+  cambiarIdioma() {
+    console.log('Cambio de idioma');
+  }
+
+  // Método para actualizar datos personales del usuario
+  actualizarDatosPersonales() {
+    console.log('Actualizar datos personales');
+    this.router.navigate(['/perfil']); // Navegar a la página de perfil para actualizar los datos personales
+  }
+
+  // Método para cambiar la contraseña
+  cambiarContrasena() {
+    console.log('Cambio de contraseña');
+    // Lógica para redirigir a una página o modal de cambio de contraseña
+  }
+
+  // Método para contactar soporte técnico
+  soporteTecnico() {
+    console.log('Contacto con soporte técnico');
+    // Lógica para contactar al soporte técnico, podría ser un modal o redirigir a otra vista
+  }
+
+  // Método para activar o desactivar el modo oscuro
+  modoOscuro(event: any) {
+    document.body.classList.toggle('dark', event.detail.checked);
+  }
+
+  // Método para cerrar la sesión
+  logout() {
+    console.log('Sesión cerrada');
+    this.router.navigate(['/login']); // Navegar a la página de inicio de sesión
+  }
 }
